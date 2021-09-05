1678295f07de262bbc643b152f9254cd539bfb6c
Kawaii <thanakrit.sat@ku.th>
2021-09-06 00:27:12 +0700

    update lab05 & lab06

--- LAB05/Pythagorean_triple.c
+++ LAB05/Pythagorean_triple.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    
+int main(){
+    long long int n,a = 3,b = 4,c = 5;
+    scanf("%lld",&n);
+    while (n>=a && n >= a+b+c )
+    {
+        long long int  b = 4;
+        while (n>=b && n >= a+b+c)
+        {
+            long long int c = n - b - a;
+            //printf("(a = %lld, b = %lld, c = %lld)\n",a,b,c);
+            if(n == a+b+c && (a*a)+(b*b) == (c*c) && a < b && b < c && a < c)
+                {
+                printf("(%lld, %lld, %lld)",a,b,c);
+                exit(0);
+                }
+            
+            b += 1;
+        }
+        a += 1;
+    }
+    printf("No triple found.");
 }
--- LAB06/ModifiedTime.c
+++ LAB06/ModifiedTime.c
@@ -5,16 +5,17 @@ int main()
 {
     int hour, min;
     char time[1];
+
     printf("Enter a 12-hour time [eg. 12:34 am]: ");
     scanf("%d:%d %c", &hour, &min, &time[0]);
+
     if (time[0] == 'A' || time[0] == 'a'){
-        if (hour == 12){
+        if (hour == 12)
             hour = 0;
-        }
     }else if (time[0] == 'P' || time[0] == 'p'){
-        if (hour < 12){
+        if (hour < 12)
             hour += 12;
-        }
     }
+    
     printf("Equivalent 24-hour time: %02d:%02d", hour, min);
 }
